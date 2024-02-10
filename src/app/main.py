944848e3ef28e54cec0b8c1a77fcbd0801563835
JPN1ETO11 <110882280+JPN1ETO11@users.noreply.github.com>
2024-02-10 11:13:44 -0500

    Update main.py

--- src/app/main.py
+++ src/app/main.py
@@ -1,52 +1,62 @@
-from app.model import PenguinModel
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import joblib
+import json
+import pandas as pd
+
+# Carga los elementos entrenados
+label_encoder = joblib.load('label_encoder.pkl')
+encoder = joblib.load('variable_encoder.pkl')
+knn_model = joblib.load('knn_model.pkl')
+lr_model = joblib.load('lr_model.pkl')
+lda_model = joblib.load('lda_model.pkl')
+scaler = joblib.load('standard_scaler.pkl')
 
 app = FastAPI()
 
-@app.on_event('startup')
-async def startup_event():
-    global penguin_model_1, penguin_model_2, penguin_model_3
-    penguin_model_1 = PenguinModel("../models/knn_model.pkl")
-    penguin_model_2 = PenguinModel("../models/lda_model.pkl")
-    penguin_model_3 = PenguinModel("../models/lr_model.pkl")
-
-class Pattern(BaseModel):
-    island: str
-    culmen_length_mm: float
-    culmen_depth_mm: float
-    flipper_length_mm: float
-    body_mass_g: float
-    sex: str
-
-class PatternModel(BaseModel):
-    model: str
-    island: str
-    culmen_length_mm: float
-    culmen_depth_mm: float
-    flipper_length_mm: float
-    body_mass_g: float
-    sex: str
-
-@app.get("/")
-def read_root():
-    return {"Hello": "This is a API to get prediction from Model"}
-
+class model_input(BaseModel):
+    model : str
+    culmenLen : float
+    culmenDepth : float
+    flipperLen : float
+    bodyMass : int
+    sex : str
+    delta15N : float
+    delta13C : float
 
 @app.post("/predict/")
-def read_item(pattern: Pattern):
-    global penguin_model_1
-    prediction = penguin_model_1.predict(pattern)
-    return {"Prediction": prediction}
+def predict(item:model_input):
 
-@app.post("/predict_model/")
-def read_item(pattern: PatternModel):
-    global penguin_model_1, penguin_model_2, penguin_model_3
-    if pattern.model == "knn":
-        return {"Prediction": penguin_model_1.predict(pattern)}
-    elif pattern.model == "lda":
-        return {"Prediction": penguin_model_2.predict(pattern)}
-    elif pattern.model == "lr":
-        return {"Prediction": penguin_model_3.predict(pattern)}
+    global label_encoder, encoder, knn_model, lr_model, lda_model, scaler
+
+    # Predice con respecto al modelo que se le especifique. 
+    X = pd.DataFrame([item.model_dump().values()], columns=item.model_dump().keys())
+
+    numerical_features = [
+    "culmenLen",
+    "culmenDepth",
+    "flipperLen",
+    "bodyMass",
+    "delta15N",
+    "delta13C"
+    ]
+    categorical_features = ["sex"]
+
+    X[numerical_features] = scaler.transform(X[numerical_features])
+    X[categorical_features] = encoder.transform(X[categorical_features])
+
+    if item.model == "knn":
+        output = knn_model.predict(X)
+        output_cod = label_encoder.inverse_transform(output).tolist()
+        return {"prediction" : output_cod}
+    elif item.model == "lda":
+        output = lda_model.predict(X)
+        output_cod = label_encoder.inverse_transform(output).tolist()
+        return {"prediction" : output_cod}
+    elif item.model == "lr":
+        output = lr_model.predict(X)
+        output_cod = label_encoder.inverse_transform(output).tolist()
+        return {"prediction" : output_cod}
     else:
         raise HTTPException(status_code=400, detail="Unsupported model")
+    
